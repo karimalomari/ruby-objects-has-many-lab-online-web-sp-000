a2caa0cbe34eca2efe55699b7cfd486cc0c10c41
Karim Alomari <karim.alomari@gmail.com>
2020-09-01 18:39:24 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,4 +8,25 @@ class Artist
     @@all << self
   end
   
+  def self.all
+    @@all
+  end
+  
+  def add_song(song)
+    song.artist = self
+  end
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+  
+  def songs 
+    Song.all.select {|song| song.artst == self}
+  end
+  
+  def self.song_count
+    Song.all.count
+  end
+  
 end
