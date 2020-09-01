65f4887b949389ff19556852295eb7096194255b
Karim Alomari <karim.alomari@gmail.com>
2020-09-01 18:40:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,4 +8,16 @@ class Song
     @@all << self
   end
   
+  def artist_name
+    if @artist == nil
+      return nil
+    else
+      @artist.name
+    end
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
