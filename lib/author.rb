b16a7311963186cbdb48cd9a44db92ff3e3f7035
Karim Alomari <karim.alomari@gmail.com>
2020-09-01 18:43:45 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,32 @@
+class Author
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+  
+  def songs 
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def self.song_count
+    Song.all.count
+  end
+  
+end
